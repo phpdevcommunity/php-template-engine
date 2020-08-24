aa686a417bba49c34b6fe72792e44d2419310a7e
Fady Michel <fadymichel.r@gmail.com>
2020-08-24 22:36:53 +0200

    change namespace

--- src/Template.php
+++ src/Template.php
@@ -1,34 +1,63 @@
 <?php
 
-namespace Webbym;
+namespace DevCoder;
 
 /**
  * Class Template
- * @package Webbym
+ * @package DevCoder
  */
 class Template
 {
+
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @var array
+     */
+    private $parameters = [];
+
+    /**
+     * Template constructor.
+     * @param string $path
+     * @param array $parameters
+     */
+    public function __construct(string $path, array $parameters = [])
+    {
+        $this->path = rtrim($path, '/').'/';
+        $this->parameters = $parameters;
+    }
+
     /**
      * @param string $view
      * @param array $context
      * @return string
      * @throws \Exception
      */
-    public function render(string $view, array $context = [])
+    public function render(string $view, array $context = []): string
     {
-        if (file_exists($view)) {
-
-            if (!empty($context)) {
-                extract($context);
-            }
-
-            ob_start();
-
-            include ($view);
-
-            return ob_get_clean();
+        if (!file_exists($view)) {
+            throw new \Exception(sprintf('The file %s could not be found.', $view));
         }
 
-        throw new \Exception(sprintf('The file %s could not be found.', $view));
+        extract(array_merge($context, ['template' => $this]));
+
+        ob_start();
+
+        include ($this->path.$view);
+
+        return ob_get_clean();
     }
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public function get(string $key)
+    {
+        return $this->parameters[$key] ?? null;
+    }
+
 }
